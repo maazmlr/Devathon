39af4b2732bceca9f85db4adc51ea31c00a43149
maazmlr <maaz70724@gmail.com>
2023-11-08 22:52:35 +0500

    update css

--- src/componnents/Doctors/Doctoer.jsx
+++ src/componnents/Doctors/Doctoer.jsx
@@ -3,12 +3,17 @@ import DocCard from "./DocCard";
 import Buttons from "./butonns";
 import axios from "axios";
 import BasicSelect from "./butonns";
+import { useSearchParams } from 'react-router-dom';
+
 
 export const Doctoer = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
     axios.get("http://localhost:3000/doctors").then((res) => setData(res.data));
   }, []);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams.get('type'))
 
   const elements = data.map((value) => (
     <DocCard
@@ -22,11 +27,12 @@ export const Doctoer = () => {
       img={value.img_url}
     />
   ));
-    console.log(data.filter(value=>value))
+    let category=(new Set(data.map(value=>value.category)))
+    category=[...category]
   return (
     <>
       <div className="button-container">
-        <BasicSelect />
+        <BasicSelect element={category}/>
       </div>
       <div
         style={{
--- src/componnents/Doctors/butonns.jsx
+++ src/componnents/Doctors/butonns.jsx
@@ -4,28 +4,39 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-
-export default function BasicSelect() {
+import { useSearchParams } from 'react-router-dom';
+export default function BasicSelect({element}) {
   const [age, setAge] = React.useState('');
+  const [searchParams, setSearchParams] = useSearchParams();
 
+  function genNewSearchParamString(key, value) {
+    const sp = new URLSearchParams(searchParams)
+    if (value === null) {
+      sp.delete(key)
+    } else {
+      sp.set(key, value)
+    }
+    return `?${sp.toString()}`
+  }
   const handleChange = (event) => {
-    setAge(event.target.value);
-  };
 
+    setAge(event.target.value);
+
+  };
+  
+  const elements=element.map((v,i)=><MenuItem onClick={()=>setSearchParams(genNewSearchParamString("type",v))} key={i} value={v}>{v}</MenuItem>)
   return (
-    <Box sx={{ minWidth: 120 }}>
+    <Box sx={{ minWidth: 123 }}>
       <FormControl className='w-2/6 '>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <InputLabel id="demo-simple-select-label">Category</InputLabel>
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
           value={age}
-          label="Age"
+          label="Category"
           onChange={handleChange}
         >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+            {elements}
         </Select>
       </FormControl>
     </Box>
