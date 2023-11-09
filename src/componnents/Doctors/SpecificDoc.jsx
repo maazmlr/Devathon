04ef60b92199834ecd1c1094b8d0050dbdbb0220
maazmlr <maaz70724@gmail.com>
2023-11-09 13:45:56 +0500

    update ui

--- src/componnents/Doctors/SpecificDoc.jsx
+++ src/componnents/Doctors/SpecificDoc.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 export const SpecificDoc = () => {
     const params=useParams()
     console.log(params.id)
+    useEffect(()=>{
+    
+    },[])
   return (
     <div>SpecificDoc</div>
   )
