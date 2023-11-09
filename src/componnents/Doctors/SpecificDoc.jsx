a0fe0e90aebb5e63e4608947effbf273d2d07045
Muhammad Maaz <compiletimeterror@gmail.com>
2023-11-09 21:17:19 +0500

    update specific

--- src/componnents/Doctors/SpecificDoc.jsx
+++ src/componnents/Doctors/SpecificDoc.jsx
@@ -1,27 +1,39 @@
 import { Button } from '@mui/material'
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+
 
 export const SpecificDoc = () => {
     const params=useParams()
-    console.log(params.id)
+    const [data,setData]=useState({})
     useEffect(()=>{
+      axios.get(`http://localhost:3000/oneDoc/${params.id}`)
+      .then(res=>setData(res.data))
     
     },[])
+    console.log(data)
   return (
     <>
     <Button variant="contained">Back to Doctors</Button>
     
-    <div className="info-container flex items-start">
-
-      <img src="https://images.unsplash.com/photo-1537368910025-700350fe46c7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt=""  className=' h-fit w-2/4'/>
-      <div className="doc-info">
-        <p className='text-4xl text-gray-900 dark:text-white'>Full Name: loem ipsunsb</p>
-        <p className='text-4xl'>DAYS: loem ipsunsb</p>
-        <p className='text-4xl'>timing: loem ipsunsb</p>
-      </div>
+    <div className="card card-side bg-base-100 shadow-xl my-10">
+  <figure><img src="https://media.istockphoto.com/id/1412215823/photo/portrait-of-confident-black-doctor.webp?b=1&s=170667a&w=0&k=20&c=OYMxE7FwQja9v4UFYWW8cX4vcLZj-vN2d12c03-kzBU=" alt="Movie"/></figure>
+  <div className="card-body">
+  <div className=" flex justify-start">
+      <h1 className='text-gray-900 text-7xl dark:text-white mb-10'>{data.category}</h1>
     </div>
+    <h2 className="card-title">Name :{data.name}</h2>
+    <h2 className="card-title"><WatchLaterIcon/> :{data.timing}</h2>
+    <h2 className="card-title"><CalendarMonthIcon/> : {data.days}</h2>
+
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Book Appointment</button>
+    </div>
+  </div>
+</div>
     </>
   )
 }
